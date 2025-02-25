f6bcde84824903ba0893367234da8eb461211ac9
Akhilkumar Boragala <42865141+akHil07090709@users.noreply.github.com>
2025-02-25 20:12:55 +0530

    #restructured code

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { createRoot } from "react-dom/client";
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
-import { createBrowserRouter, RouterProvider } from "react-router";
 import About from "./src/components/About";
+import Contact from "./src/components/Contact";
+import Error from "./src/components/Error";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const AppLayout = () => (
   <div className="app">
@@ -11,16 +13,32 @@ const AppLayout = () => (
   </div>
 );
 
-const router = createBrowserRouter([
+const object = [
+  {
+    firstName: "Akhil",
+    lastName: "Boragala",
+    address: "Huzurabad",
+    fullName: function () {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  },
+];
+
+const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <Error />,
   },
   {
     path: "/about",
     element: <About />,
   },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
 ]);
 
 const root = createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(<RouterProvider router={appRouter} />);
